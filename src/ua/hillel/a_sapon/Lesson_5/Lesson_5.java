f8303a8edcb487cc7923bad179f6db324923d8b5
icegc83 <icegc83@gmail.com>
2018-11-29 17:50:53 +0200

    Comment update

---
+++ src/ua/hillel/a_sapon/Lesson_5/Lesson_5.java
@@ -0,0 +1,14 @@
+package ua.hillel.a_sapon.Lesson_5;
+
+/**
+ * Создать класс Комплексных чисел
+ */
+
+
+public class Lesson_5 {
+    public static void main(String args[])
+    {
+        System.out.println("\n/*---------------------------------1----------------------------------------*/");
+        System.out.println("TASK 1. Create class of Complex numbers and override all required methods");
+    }
+}
